9e60bebf632d14700e42a2f788efc6662bdd40ed
sunqihang <qihang130681@gmail.com>
2023-03-08 19:07:35 +0800

    Add annotation for epoll

--- code/server/epoller.cpp
+++ code/server/epoller.cpp
@@ -7,7 +7,7 @@ Epoller::Epoller(int maxEvent):epollFd_(epoll_create(512)), events_(maxEvent){
 Epoller::~Epoller() {
     close(epollFd_);
 }
-
+// 将描述符fd加入epoll监控
 bool Epoller::AddFd(int fd, uint32_t events) {
     if(fd < 0) return false;
     epoll_event ev = {0};
@@ -15,7 +15,7 @@ bool Epoller::AddFd(int fd, uint32_t events) {
     ev.events = events;
     return 0 == epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &ev);
 }
-
+// 修改描述符fd对应的事件
 bool Epoller::ModFd(int fd, uint32_t events) {
     if(fd < 0) return false;
     epoll_event ev = {0};
@@ -23,22 +23,22 @@ bool Epoller::ModFd(int fd, uint32_t events) {
     ev.events = events;
     return 0 == epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &ev);
 }
-
+// 将描述符删除epoll的监控
 bool Epoller::DelFd(int fd) {
     if(fd < 0) return false;
     epoll_event ev = {0};
     return 0 == epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &ev);
 }
-
+// 用于返回监控的结果，成功时返回就绪的文件名描述符个数
 int Epoller::Wait(int timeoutMs) {
     return epoll_wait(epollFd_, &events_[0], static_cast<int>(events_.size()), timeoutMs);
 }
-
+// 获取fd
 int Epoller::GetEventFd(size_t i) const {
     assert(i < events_.size() && i >= 0);
     return events_[i].data.fd;
 }
-
+// 获取events
 uint32_t Epoller::GetEvents(size_t i) const {
     assert(i < events_.size() && i >= 0);
     return events_[i].events;
--- code/server/epoller.h
+++ code/server/epoller.h
@@ -14,22 +14,23 @@ public:
 
     ~Epoller();
 
+    // 将描述符fd加入epoll监控
     bool AddFd(int fd, uint32_t events);
-
+    // 修改描述符fd对应的事件
     bool ModFd(int fd, uint32_t events);
-
+    // 将描述符删除epoll的监控
     bool DelFd(int fd);
-
+    // 用于返回监控的结果，成功时返回就绪的文件名描述符个数
     int Wait(int timeoutMs = -1);
-
+    // 获取fd
     int GetEventFd(size_t i) const;
-
+    // 获取events
     uint32_t GetEvents(size_t i) const;
         
 private:
-    int epollFd_;
+    int epollFd_;   // epoll的描述符
 
-    std::vector<struct epoll_event> events_;    
+    std::vector<struct epoll_event> events_;    // 就绪的事件
 };
 
 #endif //EPOLLER_H
