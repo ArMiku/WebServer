13dc02bff3a71153104c914cd7e1d66f8d14c809
sunqihang <qihang130681@gmail.com>
2023-03-08 19:28:34 +0800

    Add annotation for buffer

--- code/buffer/buffer.cpp
+++ code/buffer/buffer.cpp
@@ -2,82 +2,91 @@
 
 Buffer::Buffer(int initBuffSize) : buffer_(initBuffSize), readPos_(0), writePos_(0) {}
 
+// 缓存区还可以读入入的字节数
 size_t Buffer::ReadableBytes() const {
     return writePos_ - readPos_;
 }
+
+// 缓存区还可以写入的字节数
 size_t Buffer::WritableBytes() const {
     return buffer_.size() - writePos_;
 }
 
+// 缓存区已经读取的字节数
 size_t Buffer::PrependableBytes() const {
     return readPos_;
 }
 
+// 获取当前读指针
 const char* Buffer::Peek() const {
     return BeginPtr_() + readPos_;
 }
 
+// 更新读指针
 void Buffer::Retrieve(size_t len) {
     assert(len <= ReadableBytes());
     readPos_ += len;
 }
 
+// 将读指针直接更新到指定位置
 void Buffer::RetrieveUntil(const char* end) {
     assert(Peek() <= end );
     Retrieve(end - Peek());
 }
 
+// 初始化读写指针
 void Buffer::RetrieveAll() {
     bzero(&buffer_[0], buffer_.size());
     readPos_ = 0;
     writePos_ = 0;
 }
 
+// 将缓冲区数据转为字符串
 std::string Buffer::RetrieveAllToStr() {
     std::string str(Peek(), ReadableBytes());
     RetrieveAll();
     return str;
 }
 
+// 获取当前写指针
 const char* Buffer::BeginWriteConst() const {
     return BeginPtr_() + writePos_;
 }
-
 char* Buffer::BeginWrite() {
     return BeginPtr_() + writePos_;
 }
 
+// 更新写指针
 void Buffer::HasWritten(size_t len) {
     writePos_ += len;
 } 
 
+// 处理http连接过程中需要的读写接口
 void Buffer::Append(const std::string& str) {
     Append(str.data(), str.length());
 }
-
 void Buffer::Append(const void* data, size_t len) {
     assert(data);
     Append(static_cast<const char*>(data), len);
 }
-
 void Buffer::Append(const char* str, size_t len) {
     assert(str);
     EnsureWriteable(len);
     std::copy(str, str + len, BeginWrite());
     HasWritten(len);
 }
-
 void Buffer::Append(const Buffer& buff) {
     Append(buff.Peek(), buff.ReadableBytes());
 }
 
+// 确保将数据写入缓冲区
 void Buffer::EnsureWriteable(size_t len) {
     if(WritableBytes() < len) {
         MakeSpace_(len);
     }
     assert(WritableBytes() >= len);
 }
-
+// 与客户端直接IO的读接口
 ssize_t Buffer::ReadFd(int fd, int* saveErrno) {
     char buff[65535];
     struct iovec iov[2];
@@ -101,7 +110,7 @@ ssize_t Buffer::ReadFd(int fd, int* saveErrno) {
     }
     return len;
 }
-
+// 与客户端直接IO的写接口
 ssize_t Buffer::WriteFd(int fd, int* saveErrno) {
     size_t readSize = ReadableBytes();
     ssize_t len = write(fd, Peek(), readSize);
@@ -112,15 +121,14 @@ ssize_t Buffer::WriteFd(int fd, int* saveErrno) {
     readPos_ += len;
     return len;
 }
-
+// 返回指向缓冲区初始位置的指针
 char* Buffer::BeginPtr_() {
     return &*buffer_.begin();
 }
-
 const char* Buffer::BeginPtr_() const {
     return &*buffer_.begin();
 }
-
+// 不够时候扩容
 void Buffer::MakeSpace_(size_t len) {
     if(WritableBytes() + PrependableBytes() < len) {
         buffer_.resize(writePos_ + len + 1);
--- code/buffer/buffer.h
+++ code/buffer/buffer.h
@@ -14,39 +14,58 @@ public:
     Buffer(int initBuffSize = 1024);
     ~Buffer() = default;
 
-    size_t WritableBytes() const;       
-    size_t ReadableBytes() const ;
+    // 缓冲区还可以读取的字节数
+    size_t WritableBytes() const;
+    
+    // 缓冲区还可以读取的字节数       
+    size_t ReadableBytes() const;
+    
+    // 缓冲区已经读取的字节数
     size_t PrependableBytes() const;
-
+    
+    // 获取当前读指针
     const char* Peek() const;
+    
+    // 确保将数据写入缓冲区
     void EnsureWriteable(size_t len);
+    // 更新写指针
     void HasWritten(size_t len);
-
+    
+    // 更新读指针
     void Retrieve(size_t len);
+    // 将读指针直接更新到指定位置
     void RetrieveUntil(const char* end);
-
-    void RetrieveAll() ;
+    
+    // 初始化读写指针
+    void RetrieveAll();
+    
+    // 将缓冲区数据转为字符串
     std::string RetrieveAllToStr();
-
+    
+    // 获取当前写指针
     const char* BeginWriteConst() const;
     char* BeginWrite();
-
+    
+    // 处理http连接过程中需要的读写接口
     void Append(const std::string& str);
     void Append(const char* str, size_t len);
     void Append(const void* data, size_t len);
     void Append(const Buffer& buff);
-
+    
+    // 与客户端直接IO的读写接口
     ssize_t ReadFd(int fd, int* Errno);
     ssize_t WriteFd(int fd, int* Errno);
 
 private:
+    // 返回指向缓冲区初始位置的指针
     char* BeginPtr_();
     const char* BeginPtr_() const;
+    // 不够时候扩容
     void MakeSpace_(size_t len);
 
-    std::vector<char> buffer_;
-    std::atomic<std::size_t> readPos_;
-    std::atomic<std::size_t> writePos_;
+    std::vector<char> buffer_;  // 存储的实体
+    std::atomic<std::size_t> readPos_;  // 当前读的位置
+    std::atomic<std::size_t> writePos_; // 当前写的位置
 };
 
 #endif //BUFFER_H
